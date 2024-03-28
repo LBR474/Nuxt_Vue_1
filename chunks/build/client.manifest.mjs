5aeb9194b943ce93f7ce716f6283f7eb50e9119a
Fake Name <fakename@fake.com>
2024-03-28 15:49:06 +1100

    Updates

---
+++ chunks/build/client.manifest.mjs
@@ -0,0 +1,69 @@
+const client_manifest = {
+  "node_modules/.pnpm/@nuxt+ui-templates@1.3.1/node_modules/@nuxt/ui-templates/dist/templates/error-404.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "DBfJ38RD.js",
+    "name": "error-404",
+    "src": "node_modules/.pnpm/@nuxt+ui-templates@1.3.1/node_modules/@nuxt/ui-templates/dist/templates/error-404.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/.pnpm/nuxt@3.11.1_@unocss+reset@0.58.7_floating-vue@5.2.2_unocss@0.58.7_vite@5.2.6/node_modules/nuxt/dist/app/entry.js"
+    ],
+    "css": []
+  },
+  "error-404.BJpHZlqW.css": {
+    "file": "error-404.BJpHZlqW.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/.pnpm/@nuxt+ui-templates@1.3.1/node_modules/@nuxt/ui-templates/dist/templates/error-500.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "BrQQAAyL.js",
+    "name": "error-500",
+    "src": "node_modules/.pnpm/@nuxt+ui-templates@1.3.1/node_modules/@nuxt/ui-templates/dist/templates/error-500.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/.pnpm/nuxt@3.11.1_@unocss+reset@0.58.7_floating-vue@5.2.2_unocss@0.58.7_vite@5.2.6/node_modules/nuxt/dist/app/entry.js"
+    ],
+    "css": []
+  },
+  "error-500.DaK1aTvB.css": {
+    "file": "error-500.DaK1aTvB.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/.pnpm/nuxt@3.11.1_@unocss+reset@0.58.7_floating-vue@5.2.2_unocss@0.58.7_vite@5.2.6/node_modules/nuxt/dist/app/entry.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "DBbyK_LY.js",
+    "name": "entry",
+    "src": "node_modules/.pnpm/nuxt@3.11.1_@unocss+reset@0.58.7_floating-vue@5.2.2_unocss@0.58.7_vite@5.2.6/node_modules/nuxt/dist/app/entry.js",
+    "isEntry": true,
+    "dynamicImports": [
+      "node_modules/.pnpm/@nuxt+ui-templates@1.3.1/node_modules/@nuxt/ui-templates/dist/templates/error-404.vue",
+      "node_modules/.pnpm/@nuxt+ui-templates@1.3.1/node_modules/@nuxt/ui-templates/dist/templates/error-500.vue"
+    ],
+    "css": [
+      "entry.CW_rqhLt.css"
+    ],
+    "_globalCSS": true
+  },
+  "entry.CW_rqhLt.css": {
+    "file": "entry.CW_rqhLt.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  }
+};
+
+export { client_manifest as default };
+//# sourceMappingURL=client.manifest.mjs.map
