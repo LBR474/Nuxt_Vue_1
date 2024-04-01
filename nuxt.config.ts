037fa82947cd46cd8624c0fdb054d3a2a6246c3c
Fake Name <fakename@fake.com>
2024-04-02 07:16:14 +1100

    add pages folder, add index, about

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  base: "/Nuxt_Vue_1"
+  baseURL: '/Nuxt_Vue_1/', // baseURL: '/<repository>/'
+    buildAssetsDir: 'assets', // don't use "_" at the begining of the folder name to avoids nojkill conflict
+ 
+
 })
